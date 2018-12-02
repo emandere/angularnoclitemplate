f4fcf154be8b79d3727c0bcc2a5f33f58b4d7303
emandere <emandere@gmail.com>
2018-12-02 03:48:11 -0500

    Initial Commit

---
+++ ClientApp/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+ selector: 'app-root',
+ template: '<h1>Hello world77!</h1>',
+})
+export class AppComponent {
+
+  constructor(){
+    console.log("I am Angular!")
+  }
+ }
